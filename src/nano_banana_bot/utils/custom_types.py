720cdbdaa17ad04be4b75bab5dabbc399572f55f
uzrama <mark.uzun7@gmail.com>
2026-03-02 00:00:09 +0300

    feat(types): add type aliases for the project

---
+++ src/nano_banana_bot/utils/custom_types.py
@@ -0,0 +1,41 @@
+from typing import TYPE_CHECKING, Annotated, Any, NewType, TypeAlias
+
+from aiogram.types import (
+    ChatMemberAdministrator,
+    ChatMemberBanned,
+    ChatMemberLeft,
+    ChatMemberMember,
+    ChatMemberOwner,
+    ChatMemberRestricted,
+    ForceReply,
+    InlineKeyboardMarkup,
+    ReplyKeyboardMarkup,
+    ReplyKeyboardRemove,
+)
+from pydantic import PlainValidator
+
+if TYPE_CHECKING:
+    ListStr: TypeAlias = list[str]
+else:
+    ListStr = NewType("ListStr", list[str])
+
+
+AnyKeyboard: TypeAlias = (
+    InlineKeyboardMarkup | ReplyKeyboardMarkup | ReplyKeyboardRemove | ForceReply
+)
+
+
+AnyChatMember: TypeAlias = (
+    ChatMemberOwner
+    | ChatMemberAdministrator
+    | ChatMemberMember
+    | ChatMemberRestricted
+    | ChatMemberLeft
+    | ChatMemberBanned
+)
+
+StringList: TypeAlias = Annotated[ListStr, PlainValidator(func=lambda x: x.split(","))]
+Int16: TypeAlias = Annotated[int, 16]
+Int32: TypeAlias = Annotated[int, 32]
+Int64: TypeAlias = Annotated[int, 64]
+DictStrAny: TypeAlias = dict[str, Any]
