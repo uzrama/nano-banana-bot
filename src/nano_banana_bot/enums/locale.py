09f1f020bdd075fb4a770b8f57b2e060dca99e45
uzrama <mark.uzun7@gmail.com>
2026-03-02 22:35:19 +0300

    fix(enums): fix names files

--- src/nano_banana_bot/enums/locale.py
+++ src/nano_banana_bot/enums/locale.py
@@ -1,29 +1,31 @@
-from enum import StrEnum
+from enum import StrEnum, auto
 
 
-class MiddlewareEventType(StrEnum):
-    MESSAGE = "message"
-    EDITED_MESSAGE = "edited_message"
-    CHANNEL_POST = "channel_post"
-    EDITED_CHANNEL_POST = "edited_channel_post"
-    BUSINESS_CONNECTION = "business_connection"
-    BUSINESS_MESSAGE = "business_message"
-    EDITED_BUSINESS_MESSAGE = "edited_business_message"
-    DELETED_BUSINESS_MESSAGES = "deleted_business_messages"
-    MESSAGE_REACTION = "message_reaction"
-    MESSAGE_REACTION_COUNT = "message_reaction_count"
-    INLINE_QUERY = "inline_query"
-    CHOSEN_INLINE_RESULT = "chosen_inline_result"
-    CALLBACK_QUERY = "callback_query"
-    SHIPPING_QUERY = "shipping_query"
-    PRE_CHECKOUT_QUERY = "pre_checkout_query"
-    PURCHASED_PAID_MEDIA = "purchased_paid_media"
-    POLL = "poll"
-    POLL_ANSWER = "poll_answer"
-    MY_CHAT_MEMBER = "my_chat_member"
-    CHAT_MEMBER = "chat_member"
-    CHAT_JOIN_REQUEST = "chat_join_request"
-    CHAT_BOOST = "chat_boost"
-    REMOVED_CHAT_BOOST = "removed_chat_boost"
-    UPDATE = "update"
-    ERROR = "error"
+class Locale(StrEnum):
+    EN = auto()  # English
+    UK = auto()  # Ukraine
+    AR = auto()  # Arabic
+    AZ = auto()  # Azerbaijani
+    BE = auto()  # Belarusian
+    CS = auto()  # Czech
+    DE = auto()  # German
+    ES = auto()  # Spanish
+    FA = auto()  # Persian
+    FR = auto()  # French
+    HE = auto()  # Hebrew
+    HI = auto()  # Hindi
+    ID = auto()  # Indonesian
+    IT = auto()  # Italian
+    JA = auto()  # Japanese
+    KK = auto()  # Kazakh
+    KO = auto()  # Korean
+    MS = auto()  # Malay
+    NL = auto()  # Dutch
+    PL = auto()  # Polish
+    PT = auto()  # Portuguese
+    RO = auto()  # Romanian
+    SR = auto()  # Serbian
+    TR = auto()  # Turkish
+    UZ = auto()  # Uzbek
+    VI = auto()  # Vietnamese
+    RU = auto()  # Russian
