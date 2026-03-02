6bf3c0c16fb7de2d2faf2d916f5c85e573df9065
uzrama <mark.uzun7@gmail.com>
2026-03-02 22:50:56 +0300

    feat(const): add const locale, timezone, rootdir, etc.

---
+++ src/nano_banana_bot/const.py
@@ -0,0 +1,17 @@
+from datetime import timezone
+from pathlib import Path
+from typing import Final
+
+from nano_banana_bot.enums.locale import Locale
+
+TIMEZONE: Final[timezone] = timezone.utc
+DEFAULT_LOCALE: Final[str] = Locale.EN
+ROOT_DIR: Final[Path] = Path(__file__).parent.parent
+ENV_FILE: Final[Path] = ROOT_DIR / ".env"
+ASSETS_SOURCE_DIR: Final[Path] = ROOT_DIR / "assets"
+MESSAGES_SOURCE_DIR: Final[Path] = ASSETS_SOURCE_DIR / "messages"
+
+# Time constants
+TIME_1M: Final[int] = 60
+TIME_5M: Final[int] = TIME_1M * 5
+TIME_10M: Final[int] = TIME_1M * 10
